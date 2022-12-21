3a532c516a506d9148f55c7a58e9f207def69993
SumanSharma <pgp10sumans@iimrohtak.ac.in>
2022-12-21 22:00:37 +0530

    oops

--- oops.py
+++ oops.py
@@ -70,16 +70,102 @@
 
 # Class attributes
 
-class Cat:
-    species = 'Mammal'
+# class Cat:
+#     species = 'Mammal'
+#
+#     def __new__(cls, *args, **kwargs):  # constructor
+#         pass
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+# c = Cat('Cat', 23)
+# print(c.species)
+#
 
-    def __new__(cls, *args, **kwargs):  # constructor
-        pass
+class Coordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __str__(self):
+        return f'Coordinate: ({self.x}, {self.y})'
 
-c = Cat('Cat', 23)
-print(c.species)
+    def distance(self, other):
+        # ((x2-x1)**2 + (y2-y1)**2)**1/2
+        d = ((self.x - other.x)**2 + (self.y - other.y)**2) ** 1/2
+        return d
+#
+# c1 = Coordinate(3, 4)
+# c2 = Coordinate(1, 2)
+#
+# print(c1.distance(c2))  # self = c1 and other = c2
+# print(c2.distance(c1))  # self = c2 and other = c1
+#
+# print(c1.distance(c1))
 
+
+# Inheritance
+
+class Animal:  # Base class
+    def __init__(self):
+        print('Animal')
+
+    def can_live_on_land(self):
+        print('yes')
+
+    def can_speak(self):
+        print('yes')
+
+
+class Monkey:
+    def __init__(self):
+        print('monkey')
+
+    def can_speak(self):
+        print("can speak monkey")
+
+    def has_legs(self):
+        print("four legs")
+
+
+class Human(Monkey, Animal):
+    h = 'Human'
+    def __init__(self):
+        print('human')
+        # self.a = a
+        # self.__b = b  # Private variable
+
+    def has_legs(self):
+        print('two legs')
+
+    def can_speak(self):
+        print('voice')
+
+a = Animal()
+m = Monkey()
+h = Human()
+
+# m.can_live_on_land()
+# m.has_legs()
+# m.can_speak()
+
+h.can_speak()
+# h.can_live_on_land()
+
+"""
+Single Inheritance
+Multi level inheritance
+hierarchical inheritance
+multiple inheritance
+
+"""
+
+## Polymorphism
+# many forms
+
+# Encapsulation
+# public and private variables
+
+# Abstraction
---
+++ test.py
@@ -0,0 +1,6 @@
+from oops import Coordinate
+
+c1 = Coordinate(0, 0)
+c2 = Coordinate(1, 2)
+
+print(c1.distance(c2))
